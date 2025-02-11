634899f0950a02a776713b39b630afa901433f26
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-02-11 16:36:54 +0100

    Successfully link tiny skia

---
+++ bl_bench/backend_tiny_skia.cpp
@@ -0,0 +1,1 @@
+#include "backend_tiny_skia.h"
---
+++ bl_bench/backend_tiny_skia.h
@@ -0,0 +1,12 @@
+#ifndef BLBENCH_BACKEND_TINY_SKIA_H
+#define BLBENCH_BACKEND_TINY_SKIA_H
+
+#include "backend.h"
+
+namespace blbench {
+
+    Backend* createTinySkiaBackend();
+
+} // {blbench}
+
+#endif // BLBENCH_BACKEND_TINY_SKIA_H
